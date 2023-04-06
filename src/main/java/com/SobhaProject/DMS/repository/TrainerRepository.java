0a6a0916a698715475585c4f1149226ba1cd1637
SR-Pittu <102750581+SR-Pittu@users.noreply.github.com>
2023-04-06 12:27:37 -0500

    first commit

---
+++ src/main/java/com/SobhaProject/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.SobhaProject.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.SobhaProject.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
